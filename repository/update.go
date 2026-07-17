4e7deb970453beedb8628b8005b90bfb92499092
engbede <emmanuelngbede74@gmail.com>
2026-07-17 18:42:09 +0100

    Add registration email, relationship and address migration

--- repository/update.go
+++ repository/update.go
@@ -14,18 +14,21 @@ func UpdateRegistration(reg models.Registration) error {
 		gender = $2,
 		age = $3,
 		phone = $4,
-		circuit = $5,
-		local_church = $6,
-		membership = $7,
-		position = $8,
-		marital_status = $9,
-		occupation = $10,
-		emergency_contact_name = $11,
-		emergency_contact_phone = $12,
-		arrival_date = $13,
-		first_time_attendee = $14,
-		bible_study_group = $15
-	WHERE id = $16
+		email = $5,
+		circuit = $6,
+		local_church = $7,
+		membership = $8,
+		position = $9,
+		marital_status = $10,
+		occupation = $11,
+		emergency_contact_name = $12,
+		emergency_contact_phone = $13,
+		relationahip = $14
+		address = &15
+		arrival_date = $16,
+		first_time_attendee = $17,
+		bible_study_group = $18
+	WHERE id = $19
 	`
 
 	_, err := database.DB.Exec(
@@ -34,6 +37,7 @@ func UpdateRegistration(reg models.Registration) error {
 		reg.Gender,
 		reg.Age,
 		reg.Phone,
+		reg.Email,
 		reg.Circuit,
 		reg.LocalChurch,
 		reg.Membership,
@@ -42,6 +46,8 @@ func UpdateRegistration(reg models.Registration) error {
 		reg.Occupation,
 		reg.EmergencyContactName,
 		reg.EmergencyContactPhone,
+		reg.Relationship,
+		reg.Address,
 		reg.ArrivalDate,
 		reg.FirstTimeAttendee,
 		reg.BibleStudyGroup,
